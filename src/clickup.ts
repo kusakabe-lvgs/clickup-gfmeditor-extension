717d2e6d971b1a1dad997964dad5f3ac2d65f8d0
kusakabe-lvgs <takuto.kusakabe@leverages.jp>
2019-06-14 17:15:46 +0900

    Change entprypoint name

---
+++ src/clickup.ts
@@ -0,0 +1,26 @@
+import App from './app';
+
+// DOMを取得のリトライ上限
+const MAX_RETRY_COUNT = 10;
+let retryCounter = 0;
+
+const findTargetList = (): any => {
+  // try数を記録する
+  retryCounter++;
+
+  const taskCards: NodeList | null = document.querySelectorAll('.cu-panel-board__clickable-card');
+
+  // max retry数を超えると止める
+  if (retryCounter >= MAX_RETRY_COUNT) {
+    clearInterval(tryGetList);
+  }
+
+  // DOMが取れたらeditorの置き換えをする
+  if (taskCards.length) {
+    const ele: HTMLDivElement = (document.querySelector('.ql-editor') as HTMLDivElement).parentNode as HTMLDivElement;
+    App(ele);
+    clearInterval(tryGetList);
+  }
+};
+
+const tryGetList = setInterval(findTargetList, 1000);
