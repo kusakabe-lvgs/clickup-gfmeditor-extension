dadfdc1d05c71f5d57f00d5e0a7a020e03a5ac03
kusakabe-lvgs <takuto.kusakabe@leverages.jp>
2019-06-18 16:17:57 +0900

    markdownでpreviewできるようにする

--- src/components/gfmEditor/preview.tsx
+++ src/components/gfmEditor/preview.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
+import { Marked } from 'marked-ts';
 
 interface IGfmEditorProps {
   gfmEditor: { value: string };
 }
 
 const GfmEditorPreview = (props: IGfmEditorProps) => {
-  return <div className="ql-editor">{props.gfmEditor.value}</div>;
+  return (
+    <div
+      className="ql-editor"
+      dangerouslySetInnerHTML={{
+        __html: Marked.parse(props.gfmEditor.value),
+      }}
+    />
+  );
 };
 
 export default GfmEditorPreview;
