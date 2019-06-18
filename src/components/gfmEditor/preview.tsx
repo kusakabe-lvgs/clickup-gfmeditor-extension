76e5fd7ffdf53612a6d192b6877456c88945fe30
kusakabe-lvgs <takuto.kusakabe@leverages.jp>
2019-06-18 15:43:09 +0900

    型つける

--- src/components/gfmEditor/preview.tsx
+++ src/components/gfmEditor/preview.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 
-const GfmEditorPreview: React.SFC = () => {
-  return <div className="ql-editor">Preview.</div>;
+interface IGfmEditorProps {
+  gfmEditor: { value: string };
+}
+
+const GfmEditorPreview = (props: IGfmEditorProps) => {
+  return <div className="ql-editor">{props.gfmEditor.value}</div>;
 };
 
 export default GfmEditorPreview;
--- src/components/gfmEditor/write.tsx
+++ src/components/gfmEditor/write.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react';
 
-const GfmEditorWrite: React.SFC = () => {
+interface IGfmEditorProps {
+  changeFormHandler: (params: string) => void;
+  gfmEditor: { value: string };
+}
+
+const GfmEditorWrite = (props: IGfmEditorProps) => {
   return (
     <div
       className="ql-editor"
       data-gram="false"
       data-placeholder='Description or type "/" for commands'
       contentEditable={true}
-    />
+      onInput={e => props.changeFormHandler((e.target as HTMLDivElement).innerText)}
+      suppressContentEditableWarning={true}
+    >
+      {props.gfmEditor.value}
+    </div>
   );
 };
 
