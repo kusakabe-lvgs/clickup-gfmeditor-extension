b8bf6698d25010b8e0e20c29eae2a09c6cf77716
kusakabe-lvgs <takuto.kusakabe@leverages.jp>
2019-06-18 18:54:23 +0900

    buttonの状態をstoreに含める

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 
 import { GfmEditorReducer } from './modules/gfmEditor';
+import { ToolbarReducer } from './modules/toolbar';
 
 const store = createStore(
   combineReducers({
     gfmEditor: GfmEditorReducer,
+    toolbar: ToolbarReducer,
   })
 );
 
