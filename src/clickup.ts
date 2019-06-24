0faf334731e49900618a93830354f924120c02db
kusakabe-lvgs <takuto.kusakabe@leverages.jp>
2019-06-24 10:44:28 +0900

    toolbarがある時だけエディタを置き換える

--- src/clickup.ts
+++ src/clickup.ts
@@ -8,7 +8,7 @@ const findTargetList = () => {
   // try数を記録する
   retryCounter++;
 
-  const taskCards: NodeList | null = document.querySelectorAll('.cu-panel-board__clickable-card');
+  const toolbar = document.querySelector('.cu-editor-toolbar') as HTMLDivElement;
 
   // max retry数を超えると止める
   if (retryCounter >= MAX_RETRY_COUNT) {
@@ -16,9 +16,8 @@ const findTargetList = () => {
   }
 
   // DOMが取れたらeditorの置き換えをする
-  if (taskCards.length) {
+  if (toolbar) {
     // toolbarは使わないので隠す
-    const toolbar = document.querySelector('.cu-editor-toolbar') as HTMLDivElement;
     toolbar.style.display = 'none';
 
     const existedEditor = document.querySelector('.ql-editor') as HTMLDivElement;
